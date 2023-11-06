142e6288113b3bc6c518a40a9adfc668e60d4869
mindae <mahendrachaurasia@gmail.com>
2023-11-06 21:10:29 +0530

    updated 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        System.out.println("Welcome World");
+        System.out.println("f3 Welcome World");
     }
 
     public static void printSomething() {
-        System.out.println("Print something");
+        System.out.println("Print something!");
     }
 
     public static void doSomething() {
-        System.out.println("do something");
+        System.out.println("f3 do something");
     }
 }
