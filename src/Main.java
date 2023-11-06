5312378a3973e10b52368e6a197078430933d961
mindae <mahendrachaurasia@gmail.com>
2023-11-06 14:55:23 +0530

    8

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
         System.out.println(newnum + 5);
         System.out.println(newnum + 3);
         System.out.println(newnum + 6);
-        System.out.println(newnum + 7777);
+        System.out.println(newnum + 8888);
 
         Stream.of("hello", "world!","hello")
                 .collect(Collectors.toMap(Function.identity(), String::length, (e, n) -> e))
