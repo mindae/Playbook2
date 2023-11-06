e5df62e6642ece8073d7c73c592d523d16868147
mindae <mahendrachaurasia@gmail.com>
2023-11-06 20:40:41 +0530

    updated fn 1,3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome World");
     }
 
     public static void printSomething() {
@@ -8,6 +8,6 @@ public class Main {
     }
 
     public static void doSomething() {
-        System.out.println("do something");
+        System.out.println("please do something");
     }
 }
