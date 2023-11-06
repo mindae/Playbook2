6753314ffb7199d353d8b28d9065cc4008984ac0
mindae <mahendrachaurasia@gmail.com>
2023-11-06 20:12:01 +0530

    new

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,9 @@
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
-        int num = 1;
-        System.out.println(num);
-        String str = "hellodebug";
-        System.out.println(str);
-
-        int newnum = 1+100;
-        System.out.println(newnum + 1);
-
-        System.out.println(newnum + 1);
-        System.out.println(newnum + 1);
-
-        System.out.println(newnum + 2);
-        System.out.println(newnum + 2);
-
-        System.out.println(newnum + 3);
-        System.out.println(newnum + 5);
-        System.out.println(newnum + 3);
-        System.out.println(newnum + 6);
-        System.out.println(newnum + 8887);
-
-        Stream.of("hello", "world!","hello")
-                .collect(Collectors.toMap(Function.identity(), String::length, (e, n) -> e))
-                .forEach((k, v) -> System.out.println(k + ": " + v));
+    public static void printSomething() {
+        System.out.println("Print something");
     }
 }
